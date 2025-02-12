2e7dba231fac3c11a46348f257483bb4449e3031
Saleh Akram Sifat <salehakramsifat@gmail.com>
2025-02-12 23:57:27 +0600

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,16 @@ class HomeActivity extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(appBar: AppBar(title: Text("Navigation Widget"),centerTitle: true,),
+      drawer: Drawer(child: ListView(padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(decoration: BoxDecoration(color: Colors.blue,),
+            child: Text("Menu", style: TextStyle(color: Colors.white, fontSize: 20),),),
+
+          ListTile(leading: Icon(Icons.home),title: Text("Home"),onTap: (){Navigator.pop(context);},),
+          ListTile(leading: Icon(Icons.person),title: Text("Person"),),
+          ListTile(leading: Icon(Icons.person_pin),title: Text("Profile"),),
+          ListTile(leading: Icon(Icons.exit_to_app),title: Text("Exit"),onTap: (){Navigator.pop(context);},)
+        ],)),
 
     );
   }
