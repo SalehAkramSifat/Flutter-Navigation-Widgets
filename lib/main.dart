7ea7bdd7f96c9bba5142623a3c528f78cbd0a755
Saleh Akram Sifat <salehakramsifat@gmail.com>
2025-02-12 23:56:43 +0600

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'dart:js';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home:HomeActivity());
+  }
+}
+class HomeActivity extends StatelessWidget{
+  const HomeActivity({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text("Navigation Widget"),centerTitle: true,),
+
+    );
+  }
+}
